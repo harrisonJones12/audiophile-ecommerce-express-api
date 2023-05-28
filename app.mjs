fcf0addc9c99adc6869dfe8b176eef38d3420cfb
Harrison Jones <harrisonjones5678@gmail.com>
2023-05-28 17:41:48 -0400

    Connect mongo db (#1)

---
+++ app.mjs
@@ -0,0 +1,22 @@
+// const routes = require('./routes/index');
+import express from "express";
+import cors from "cors"
+
+import products from './routes/products.mjs';
+
+const PORT = process.env.PORT || 5050;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/", products)
+
+app.use((err, _req, res, next) => {
+    res.status(500).send("Uh oh! An unexpected error occured")
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running at port ${PORT}`)
+})
